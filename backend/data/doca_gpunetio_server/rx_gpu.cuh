94190ae0fea0afd5bd3e2ac6dd946e4113dc21e4
Rex Tse <yltseaf@connect.ust.hk>
2026-04-20 23:54:41 +0800

    doca gpunetio retry

--- backend/data/doca_gpunetio_server/rx_gpu.cuh
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "doca_context.hpp"
-
-namespace snakeio::doca_gpunetio_server {
-    bool start_rx_worker(transport_context& ctx) noexcept;
-    void stop_rx_worker(transport_context& ctx) noexcept;
-}
-
-
--- backend/data/doca_gpunetio_server/tx_gpu.cuh
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "doca_context.hpp"
-
-namespace snakeio::doca_gpunetio_server {
-    bool start_tx_worker(transport_context& ctx) noexcept;
-    void stop_tx_worker(transport_context& ctx) noexcept;
-}
-
